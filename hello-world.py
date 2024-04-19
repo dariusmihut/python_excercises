3688fbc3a72ca6629982c354543efeef546382cc
Darius Mihut (ext.) <darius.dm.mihut@partner.bmwgroup.com>
2024-04-19 13:51:56 +0300

    added basic encoder/decoder function

--- hello-world.py
+++ hello-world.py
@@ -1,5 +1,26 @@
-def main():
-  print("Hello world!")
+coded_message = 'DrsCisCikxyDroBiwoCCkqoiDyiloiMYNONjicXPY,bcXKbOVgibRSaieKaiXYbiMYNON'
+plain_message = 'this is another message to be CODED, UNFORTUNATELY THIS WAS NOT CODED'
+alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ,'
 
-if __name__ == "__main__":
-  main()
+
+def messageCoding(message, shift_lenght, coding=True):
+    modified_message = ''
+    for chr in message:
+        if chr in alphabet:
+            if coding:
+                index = alphabet.find(chr) + shift_lenght
+            else:
+                index = alphabet.find(chr) - shift_lenght
+            if index >= len(alphabet):
+                index = index - len(alphabet)
+
+            modified_message += alphabet[index]
+
+        else:
+            modified_message += chr
+
+    else:
+        return 'encoded message: {}'.format(modified_message)
+
+
+print(messageCoding(coded_message, 10, coding=False))
